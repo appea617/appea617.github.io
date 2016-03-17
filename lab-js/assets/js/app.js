c2e75cfc724871a9a396beb793a73138eb61d262
amon <appea617@newschool.edu>
2016-03-17 07:33:04 -0400

    styled lab-js

--- lab-js/assets/js/app.js
+++ lab-js/assets/js/app.js
@@ -1,69 +1,50 @@
+// function
+//alert("hello js");
 
-
-
-//function
-//alert('hello js');
-
-//variables
+//variables 
 var animal = "cat";
-var age = 14;
-var clothing =["shirts", "socks", "pants"];
-var winner = false;
-var checking_account= 1000;
-var marni_shirt=1000;
-var marni_earrings=450;
-var stella_dress=3000;
+var age = 13;
+var clothing = ["shirts", "socks", "pants"];
+var winner = false; 
+var checking_account = 1000;
 
 console.log(winner);
 
 
-// functions
+//functions 
 function print_text(text) {
-	$('.value').html(text);
+	$(".value").html(text);
 }
 
 
+function charge_cc(item_value, brand_name) {
+	alert("you will be charged " + item_value + " for this " + brand_name + " item");
+	alert("your checking account balance is " + checking_account);
 
-function charge_cc(item_value, brand_name){
-	alert('You will be charged'+  item_value + 'for this' + brand_name +'item');
-	alert('Your checking account balance is'+  checking_account);
 
-	
 	if (checking_account < item_value) {
 		if (checking_account < 1) {
-			alert('you do not have any money');
-		}else{
-		alert('you do not have enough money to purchase this item');
+		  alert("You don't have any money left");
+		} else {
+		  alert("You don't have enought money to purchase this item");
 		}
-		}else{
-	checking_account =  checking_account-item_value;
-	alert('charge successful');
-	display_checking_balance();
-	alert('checking balance is'+  checking_account);
-}
-}
-
-function display_checking_balance(){
-	$('.value').html(checking_account);
+	} else {
+		checking_account = checking_account - item_value;
+		alert("charge successful");
+		display_checking_balance();
+		alert("checking balance is " + checking_account);
+	}
 }
 
-$('.item').click(function(){
+function display_checking_balance() {
+	$(".value").html(checking_account);
+}
 
-	var item_value = $(this).data('value');
-	var brand_name = $(this).data('brand');
-
-	charge_cc(item_value);
-
+$(".item").click(function() {
+	var item_value = $(this).data("value");
+	var brand_name = $(this).data("brand");
+	charge_cc(item_value, brand_name);
 });
 
-
-
-
-
-
-
-
-
-
-
+display_checking_balance();
 
